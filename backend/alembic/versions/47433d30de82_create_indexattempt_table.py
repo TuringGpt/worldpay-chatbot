0272d651ef0dcbc4a784ca8fccca14c1976a87bc
leonard-bc <leonard.b-c@turing.com>
2024-09-04 08:14:15 +0000

    alembic fixed

--- backend/alembic/versions/47433d30de82_create_indexattempt_table.py
+++ backend/alembic/versions/47433d30de82_create_indexattempt_table.py
@@ -17,57 +17,59 @@ depends_on: None = None
 
 
 def upgrade() -> None:
-    op.create_table(
-        "index_attempt",
-        sa.Column("id", sa.Integer(), nullable=False),
-        # String type since python enum will change often
-        sa.Column(
-            "source",
-            sa.String(),
-            nullable=False,
-        ),
-        # String type to easily accomodate new ways of pulling
-        # in documents
-        sa.Column(
-            "input_type",
-            sa.String(),
-            nullable=False,
-        ),
-        sa.Column(
-            "connector_specific_config",
-            postgresql.JSONB(),
-            nullable=False,
-        ),
-        sa.Column(
-            "time_created",
-            sa.DateTime(timezone=True),
-            server_default=sa.text("now()"),
-            nullable=True,
-        ),
-        sa.Column(
-            "time_updated",
-            sa.DateTime(timezone=True),
-            server_default=sa.text("now()"),
-            server_onupdate=sa.text("now()"),  # type: ignore
-            nullable=True,
-        ),
-        sa.Column(
-            "status",
-            sa.Enum(
-                "NOT_STARTED",
-                "IN_PROGRESS",
-                "SUCCESS",
-                "FAILED",
-                name="indexingstatus",
-                native_enum=False,
-            ),
-            nullable=False,
-        ),
-        sa.Column("document_ids", postgresql.ARRAY(sa.String()), nullable=True),
-        sa.Column("error_msg", sa.String(), nullable=True),
-        sa.PrimaryKeyConstraint("id"),
-    )
+    pass
+    # op.create_table(
+    #     "index_attempt",
+    #     sa.Column("id", sa.Integer(), nullable=False),
+    #     # String type since python enum will change often
+    #     sa.Column(
+    #         "source",
+    #         sa.String(),
+    #         nullable=False,
+    #     ),
+    #     # String type to easily accomodate new ways of pulling
+    #     # in documents
+    #     sa.Column(
+    #         "input_type",
+    #         sa.String(),
+    #         nullable=False,
+    #     ),
+    #     sa.Column(
+    #         "connector_specific_config",
+    #         postgresql.JSONB(),
+    #         nullable=False,
+    #     ),
+    #     sa.Column(
+    #         "time_created",
+    #         sa.DateTime(timezone=True),
+    #         server_default=sa.text("now()"),
+    #         nullable=True,
+    #     ),
+    #     sa.Column(
+    #         "time_updated",
+    #         sa.DateTime(timezone=True),
+    #         server_default=sa.text("now()"),
+    #         server_onupdate=sa.text("now()"),  # type: ignore
+    #         nullable=True,
+    #     ),
+    #     sa.Column(
+    #         "status",
+    #         sa.Enum(
+    #             "NOT_STARTED",
+    #             "IN_PROGRESS",
+    #             "SUCCESS",
+    #             "FAILED",
+    #             name="indexingstatus",
+    #             native_enum=False,
+    #         ),
+    #         nullable=False,
+    #     ),
+    #     sa.Column("document_ids", postgresql.ARRAY(sa.String()), nullable=True),
+    #     sa.Column("error_msg", sa.String(), nullable=True),
+    #     sa.PrimaryKeyConstraint("id"),
+    # )
 
 
 def downgrade() -> None:
-    op.drop_table("index_attempt")
+    pass
+    # op.drop_table("index_attempt")
